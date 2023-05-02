0248a511905de202b3955ff7339defb052b3cd50
Michel Hoffmann <mic.hoff.jr@gmail.com>
2023-05-01 19:52:37 -0500

    .

--- SearchGames/assets/js/main.js
+++ SearchGames/assets/js/main.js
@@ -1,13 +1,23 @@
-const input = document.querySelector("#input");
+let input = document.querySelector("#input");
 const cards = document.querySelectorAll(".card");
 
-input.addEventListener("keypress", filterCards);
+input.addEventListener("input", filterCards);
 
 function filterCards() {
-  for (const card of cards) {
-    const h1 = card.querySelector(".titulo-game");
-    if (input.innerText.includes(h1.innerText)) {
-      console.log("finalmente");
+  if (input.value != '') {
+    for (let card of cards) {
+      let titulo = card.querySelector('.titulo-game')
+      titulo = titulo.innerText.toLowerCase()
+      let inputText = input.value.toLowerCase()
+      if (!titulo.includes(inputText)) {
+        card.style.display = 'none'
+      } else {
+        card.style.display = "block";
+      }
+    }
+  } else {
+    for (let card of cards) {
+      card.style.display = 'block'
     }
   }
 }
